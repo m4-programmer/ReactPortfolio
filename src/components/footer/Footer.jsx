15e6c7a03e4e7a1ccd132f18c1265a462b1e2b3f
m4-programmer <miraboy13@gmail.com>
2022-09-20 06:55:04 +0100

    Updated my socail links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,7 @@ import { FaFacebookF } from 'react-icons/fa'
 import { FaInstagram } from 'react-icons/fa'
 import { IoLogoTwitter} from 'react-icons/io'
 import { FaWhatsapp } from 'react-icons/fa'
+import {AiFillLinkedin} from 'react-icons/ai'
 const Footer = () => {
   return (
     <footer id='footer'>
@@ -20,10 +21,11 @@ const Footer = () => {
       </ul>
 
       <div className="footer_socials">
-        <a href="https://facebook.com"><FaFacebookF /> </a>
-        <a href="https://instagram.com"><FaInstagram /> </a>
-        <a href="https://twitter.com"><IoLogoTwitter /> </a>
-        <a href="https://whatsapp.com"><FaWhatsapp /> </a>
+        <a href="#"><FaFacebookF /> </a>
+        <a href="https://instagram.com/adolphusmiracle"><FaInstagram /> </a>
+        <a href="https://twitter.com/mine679"><IoLogoTwitter /> </a>
+        <a href="https://api.whatsapp.com/send?phone=+2349093386514"><FaWhatsapp /> </a>
+        <a href="https://linkedin.com/in/adolphus-miracle"><AiFillLinkedin /></a>
       </div>
 
       <div className="footer_copyright">
